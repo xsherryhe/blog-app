77e8917778f5fbc8659a38b68b10b2ab02229eac
Sherry He <xsherryhe@gmail.com>
2022-07-26 12:53:20 -0700

    Completed Section 10 of Getting Started guide

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to @article
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @article, status: :see_other
+  end
+
   private
 
   def comment_params
